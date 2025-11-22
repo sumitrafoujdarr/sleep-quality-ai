58a2a0c4c36003b533ba3d88e657012ca9a90d7d
Sumitra Foujdar <sumitrafoujdarr@gmail.com>
2025-11-23 00:42:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from auth import authentication_controller, logout_button
 from sleep_app import show_sleep_app
 
-st.set_page_config(page_title="Sleep Analyzer", page_icon="😴")
+st.set_page_config(page_title="Sleep Analyzer", page_icon="🌙")
 
 # AUTHENTICATION SYSTEM
 logged_in = authentication_controller()
